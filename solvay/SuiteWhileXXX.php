8e1869a59baff3e334febec12d11971b909a8e99
Alexandre Lenaerts <13055@ecam.be>
2017-03-31 02:36:59 +0200

    Final

---
+++ solvay/SuiteWhileXXX.php
@@ -0,0 +1,19 @@
+<?php>
+	$i = 0;
+	$pair = array();
+	$impair = array();
+	while(i<rand(0,1000){
+		if (i%2==0)
+		{
+			array_push($pair,$i)
+		}
+		array_push($impair,$i)
+	}
+	echo $impair:
+	echo $pair;
+	?>
+
+
+
+
+
